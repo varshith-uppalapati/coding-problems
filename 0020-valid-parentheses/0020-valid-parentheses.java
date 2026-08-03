bec683f729eefef02d6e70bd570e1807705253ab
Uppalapati Varshith Varma <2410030413@klh.edu.in>
2026-08-03 23:01:54 +0530

    Time: 2 ms (98.24%), Space: 43 MB (86.24%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stk = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch =='[' || ch=='(' || ch=='{'){
+                stk.push(ch);
+            }
+            else{
+                if(stk.isEmpty()) return false;
+            char top =stk.pop();
+            if(ch == ']' && top!='[') return false;
+            if(ch ==')' && top!='(') return false;
+            if(ch =='}' && top!='{') return false;
+
+        }
+        }
+        return stk.isEmpty();
+}
+}
